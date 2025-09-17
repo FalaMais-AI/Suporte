08e10829c651395d1fe08a6b5e45760bba5eb351
Arthur <arthurssousa2004@gmail.com>
2025-09-16 22:10:48 -0300

    deploy

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://FalaMais-AI.github.io', // URL base do seu GitHub Pages
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Suporte/ ',
+  baseUrl: '/Suporte/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
